bcfb1835d2db6e9fcac8f4b99a36599ad8c56900
bobur <uzxonboy@hotmail.com>
2019-10-19 17:30:49 -0400

    minor fixes

--- votesys/src/Components/SignIn/SignIn.js
+++ votesys/src/Components/SignIn/SignIn.js
@@ -12,81 +12,126 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import {Redirect} from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import './SignIn.css';
 
-export default class Signin extends React.Component{
+const styles = theme => ({
+  '@global': {
+      body: {
+          backgroundColor: theme.palette.common.white,
+      },
+  },
+  paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+  },
+  avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+  },
+  submit: {
+      margin: theme.spacing(3, 0, 2),
+  },
+});
+
+class SignIn extends React.Component{
   constructor(props)
   {
       super(props);
+      this.state = {
+        email: "",
+        password:""
+    };
   }
+
+  handleChange = event => ({target}) => {
+    this.setState({[event]: target.value});
+  }
+
+  handleSubmit = async (event) => {
+
+  }
+
+  handleKeyPress = event => {
+    if(event.key === 'Enter') {
+      this.handleSubmit();
+    }
+  }
+
   render(){
+    const {classes} = this.props
     if(localStorage.token)
     {
       return (<Redirect to="/main" />);
     }
     return (
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div> 
-          {/* className={classes.paper}> */}
-          <Typography component="h1" color="primary" variant="h3" align="center">
-            ChoiceVote
+          <CssBaseline />
+          <div className={classes.paper}>
+              <Typography component="h1" color='primary' variant="h4">
+                  ChoiceVote
           </Typography>
-          <br/><br/><br/>
-          {/* <Avatar>
-            className={classes.avatar}>
-            <LockOutlinedIcon align="center"/>
-          </Avatar> */}
-          {/* <Typography component="h1" color="primary" variant="h6" align="center">
-            Claim your free public key
-          </Typography> */}
-          <form >
-            {/* className={classes.form} noValidate> */}
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              //className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  Don't have an account? Sign up
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        {/* <Box mt={5}>
-          <Copyright />
-        </Box> */}
+              <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                  Sign in
+              </Typography>
+              <form className={classes.form} noValidate>
+                  <TextField
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      placeholder='.cuny.edu'
+                      autoComplete="email"
+                      autoFocus
+                      onChange={this.handleChange('email')}
+                  />
+                  <TextField
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                      onChange={this.handleChange('password')}
+                  />
+                  <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
+                      //onClick={this.handleSubmit}
+                  >
+                      Sign In
+                  </Button>
+                  <Grid container>
+                      <Grid item>
+                          <Link href="/signup" variant="body2">
+                              Don't have an account? Sign Up
+                          </Link>
+                      </Grid>
+                  </Grid>
+              </form>
+              {/* {this.state.error.length > 0 ? (<h3 style={{ color: 'Red' }}>{this.state.error}</h3>) : (<div></div>)} */}
+          </div>
+          <div>
+          </div>
       </Container>
-    );
+    )
   }
 }
+
+export default withStyles(styles)(SignIn);
--- votesys/src/Components/SignUp/SignUp.js
+++ votesys/src/Components/SignUp/SignUp.js
@@ -14,80 +14,120 @@ import Container from '@material-ui/core/Container';
 import { withStyles } from '@material-ui/core/styles';
 // import "./Register.css";
 
+const styles = theme => ({
+  '@global': {
+      body: {
+          backgroundColor: theme.palette.common.white,
+      },
+  },
+  paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+  },
+  avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+  },
+  submit: {
+      margin: theme.spacing(3, 0, 2),
+  },
+});
 
-export default class SignUp extends React.Component {
+class SignUp extends React.Component {
   constructor(props) {
     super();
     this.state = {
-        email: "",
-        password:""
+      email: "",
+      password:""
     };
   }
+
+  handleChange = event => ({target}) => {
+    this.setState({[event]: target.value});
+  }
+
+  handleSubmit = async (event) => {
+
+  }
+
+  handleKeyPress = event => {
+    if(event.key === 'Enter') {
+      this.handleSubmit();
+    }
+  }
+
   render() {
     //if (this.state.isAuthenticated === true) return <Redirect to="/" />;
+    const {classes} = this.props 
     return (
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div> 
-          {/* className={classes.paper}> */}
-          <Typography component="h1" color="primary" variant="h3" align="center">
-            ChoiceVote
+          <CssBaseline />
+          <div className={classes.paper}>
+          <Typography component="h1" color = 'primary' variant="h4">
+                  ChoiceVote
           </Typography>
-          <br/><br/><br/>
-          {/* <Avatar>
-            className={classes.avatar}>
-            <LockOutlinedIcon align="center"/>
-          </Avatar> */}
-          {/* <Typography component="h1" color="primary" variant="h6" align="center">
-            Claim your free public key
-          </Typography> */}
-          <form >
-            {/* className={classes.form} noValidate> */}
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              //className={classes.submit}
-            >
-              Claim your free public key
-            </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link href="/" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        {/* <Box mt={5}>
-          <Copyright />
-        </Box> */}
+              <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                  Sign up
+              </Typography>
+              <form className={classes.form} noValidate>
+                  <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                          <TextField
+                              variant="outlined"
+                              fullWidth
+                              id="email"
+                              label="Email Address"
+                              placeholder='.cuny.edu'
+                              name="email"
+                              autoComplete="email"
+                              onChange={this.handleChange('email')}
+                          />
+                      </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              variant="outlined"
+                              fullWidth
+                              name="password"
+                              label="Password"
+                              type="password"
+                              id="password"
+                              autoComplete="current-password"
+                              onChange={this.handleChange('password')}
+                          />
+                      </Grid>
+                  </Grid>
+                  <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
+                      onClick={this.handleSubmit}
+                  >
+                      Claim Your Free Public Key
+                  </Button>
+                  <Grid container justify="flex-end">
+                      <Grid item>
+                          <Link href="/" variant="body2">
+                              Already have an account? Sign in
+                          </Link>
+                      </Grid>
+                  </Grid>
+              </form>
+              <h3 style={{color:'Red'}}>{this.state.error}</h3>
+          </div>
       </Container>
-    );
+    )
   }
 }
+
+export default withStyles(styles)(SignUp);
